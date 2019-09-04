415bafe3b768093515bd07d6cb0466079377e2e5
Jiangran Wang <wjr@wangjiangrandeMacBook-Air.local>
2019-09-03 22:08:00 -0500

    add noise

--- Batch.py
+++ Batch.py
@@ -7,39 +7,54 @@ f_test = h5py.File("data/TestEOB_q-1-10-0.02_ProperWhitenZ.h5", "r")
 
 NUM_DATA = f_train['WhitenedSignals'].shape[0]
 LENGTH = f_train['WhitenedSignals'].shape[1]
-def get_batch(f,k,length=LENGTH): 
-    batch = []
-    label = []
-    idx = np.arange(NUM_DATA)
-    np.random.shuffle(idx)
-    noise = Noiser()
+def get_batch(f,k,length=LENGTH,real_noise=False,SNR=None): 
+	batch = []
+	label = []
+	idx = np.arange(NUM_DATA)
+	np.random.shuffle(idx)
+	noise = Noiser()
 
-    num_batch = NUM_DATA//k
-    for i in range(num_batch):
-        cur_batch = []
-        cur_label = []
-        for j in range(k):
-            cur_batch.append(f['WhitenedSignals'][idx[k*i+j]][:length])
-            cur_label.append(f['m1m2'][idx[k*i+j]])
-        cur_batch = noise.add_shift(cur_batch)
-        batch.append(cur_batch)
-        label.append(cur_label) 
-        
-    batch = np.asarray(batch).reshape(num_batch,k,length,1)
-    label = np.asarray(label)
-    return batch, label
+	num_batch = NUM_DATA//k
+	snr = np.random.uniform(low=SNR,high=40.0,size=num_batch)
+	print(snr)
+	for i in range(num_batch):
+		cur_batch = []
+		cur_label = []
+		for j in range(k):
+			cur_batch.append(f['WhitenedSignals'][idx[k*i+j]][:length])
+			cur_label.append(f['m1m2'][idx[k*i+j]])
+		cur_batch = noise.add_shift(cur_batch)
+		if SNR is not None:
+			if real_noise is False:
+				cur_batch = noise.add_noise(input=cur_batch, SNR=snr[i])
+			else:
+				cur_batch = noise.add_real_noise(input=cur_batch, SNR=snr[i])
+		batch.append(cur_batch)
+		label.append(cur_label) 
+		
+	batch = np.asarray(batch).reshape(num_batch,k,length,1)
+	label = np.asarray(label)
+	return batch, label
 
-def get_val(f,k,length=LENGTH):
-    batch = []
-    label = []
-    idx = np.random.choice(f_test['WhitenedSignals'].shape[0], k, replace=False)
-    noise = Noiser()
+def get_val(f,k,length=LENGTH,real_noise=False,SNR=None):
+	batch = []
+	label = []
+	idx = np.random.choice(f_test['WhitenedSignals'].shape[0], k, replace=False)
+	idx = [100]
+	noise = Noiser()
 
-    for i in range(k):
-        batch.append(f['WhitenedSignals'][idx[i]][:length])
-        label.append(f['m1m2'][idx[i]])
+	for i in range(k):
+		batch.append(f['WhitenedSignals'][idx[i]][:length])
+		label.append(f['m1m2'][idx[i]])
 
-    batch = noise.add_shift(batch)
-    batch = np.asarray(batch).reshape(k,length,1)
-    label = np.asarray(label)
-    return batch, label
+	if SNR is not None:
+		snr = np.random.uniform(low=SNR,high=SNR,size=1)[0]            
+		print(snr)
+		if real_noise is False:
+			batch = noise.add_noise(input=batch, SNR=snr)
+		else:
+			batch = noise.add_real_noise(input=batch, SNR=snr)
+	batch = noise.add_shift(batch)
+	batch = np.asarray(batch).reshape(k,length,1)
+	label = np.asarray(label)
+	return batch, label
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+import math
 from Noiser import Noiser
 from Net import WaveNet
 from Batch import get_batch, get_val
@@ -24,15 +25,15 @@ loss = tf.losses.mean_squared_error(input_label, predictions)
 # train operation
 global_step = tf.Variable(0, trainable=False)
 learning_rate = tf.train.exponential_decay(learning_rate=0.001, 
-                                           global_step=global_step, 
-                                           decay_steps=9861//64, 
-                                           decay_rate=0.96, 
-                                           staircase=True)
+										   global_step=global_step, 
+										   decay_steps=9861//64, 
+										   decay_rate=0.96, 
+										   staircase=True)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(
-    loss=loss,
-    global_step=global_step)
+	loss=loss,
+	global_step=global_step)
 
 #initialization
 init = tf.global_variables_initializer()
@@ -43,33 +44,40 @@ loss_hist = []
 val_loss = []
 #saver.restore(sess, "../model/shift.ckpt")
 
-num_epoch = 500
+num_epoch = 5000
 start = datetime.datetime.now()
 batch_size = 64
+real_noise = False  #change here!
 rate = 0.001
 for i in range(num_epoch):
-    train_data, train_label = get_batch(f_train, batch_size)
-    for j in range(len(train_data)):
-        cur_data = train_data[j]
-        cur_label = train_label[j]
-        _, loss_val = sess.run([train_op, loss],
-                           feed_dict={input_data: cur_data,
-                                      input_label: cur_label,
-                                      feedlr: rate})
-        loss_hist.append(loss_val)
-        if j % 10 == 0:
-            print('loss: '+str(loss_hist[-1]))
-    
-    val_data, val_label = get_val(f_test, batch_size)
-    validation = sess.run(loss, feed_dict={input_data: val_data, input_label: val_label})
-    val_loss.append(validation)
-    print('iter num: '+str(i)+' loss: '+str(loss_hist[-1])+' val_loss: '+str(val_loss[-1]))
-
+	snr = 40.0*math.exp(np.float64(i)*(-0.001))
+	if snr < 1.5:
+		snr = 1.5
+	# global_step.eval(session=sess)
+	train_data, train_label = get_batch(f_train, batch_size, real_noise=real_noise, SNR=snr)
+	for j in range(len(train_data)):
+		cur_data = train_data[j]
+		cur_label = train_label[j]
+		_, loss_val = sess.run([train_op, loss],
+						   feed_dict={input_data: cur_data,
+									  input_label: cur_label,
+									  feedlr: rate})
+		loss_hist.append(loss_val)
+		if j % 10 == 0:
+			print('loss: '+str(loss_hist[-1]))
+	
+	val_data, val_label = get_val(f_test, batch_size, real_noise=real_noise, SNR=snr)
+	validation = sess.run(loss, feed_dict={input_data: val_data, input_label: val_label})
+	val_loss.append(validation)
+	print('iter num: '+str(i)+' snr: '+str(snr)+' loss: '+str(loss_hist[-1])+' val_loss: '+str(val_loss[-1]))
+	if i % 500 == 0:
+		saver.save(sess, '../model/'+str(real_noise)+'_Rnoise.ckpt', global_step=i)
+	
 end = datetime.datetime.now()
 print('time: '+str(end-start))
 
 #save model
-save_path = saver.save(sess, "../model/shift.ckpt")
+save_path = saver.save(sess, '../model/'+str(real_noise)+'_Rnoise.ckpt', global_step=num_epoch)
 print("Model saved in path: %s" % save_path)
 
 def showplot(pred,name):
@@ -80,14 +88,14 @@ def showplot(pred,name):
 	cm = plt.cm.get_cmap('seismic')
 	plt.subplot(211)
 	sc = plt.scatter(test_label.T[0], test_label.T[1], c=error1, vmin=0.0025, vmax=0.75, 
-	                 cmap=cm, norm=colors.LogNorm(vmin=np.amin(error1), vmax=np.amax(error1)))
+					 cmap=cm, norm=colors.LogNorm(vmin=np.amin(error1), vmax=np.amax(error1)))
 	plt.colorbar(sc)
 	plt.xlabel('m1 mass')
 	plt.ylabel('m2 mass')
 	plt.title(name)
 	plt.subplot(212)
 	sc = plt.scatter(test_label.T[0], test_label.T[1], c=error2, vmin=0.0025, vmax=0.75, 
-	                 cmap=cm, norm=colors.LogNorm(vmin=np.amin(error2), vmax=np.amax(error2)))
+					 cmap=cm, norm=colors.LogNorm(vmin=np.amin(error2), vmax=np.amax(error2)))
 	plt.colorbar(sc)
 	plt.xlabel('m1 mass')
 	plt.ylabel('m2 mass')
@@ -98,29 +106,39 @@ def showplot(pred,name):
 start = 0
 end = 8192
 
-pred = []
-for i in range(len(f_test['WhitenedSignals'])):
-    test_data = f_test['WhitenedSignals'][i][start:end].reshape(1,end-start,1)
-    test_label = f_test['m1m2'][i].reshape(1,2)
-    pred.append(sess.run(predictions, feed_dict={input_data: test_data, input_label: test_label})[0])
-pred = np.asarray(pred)
-test_label = np.asarray(f_test['m1m2'])
-m1 = np.mean(np.divide(abs(pred.T[0]-test_label.T[0]),test_label.T[0]))
-m2 = np.mean(np.divide(abs(pred.T[1]-test_label.T[1]),test_label.T[1]))
-print('without shift--m1: '+str(m1)+' m2: '+str(m2))
-showplot(pred,'noshift')
-
-pred = []
 noise = Noiser()
-for i in range(len(f_test['WhitenedSignals'])):
-    test_data = f_test['WhitenedSignals'][i][start:end].reshape(1,end-start)
-    test_data = noise.add_shift(test_data)
-    test_data = test_data.reshape(1,end-start,1)
-    test_label = f_test['m1m2'][i].reshape(1,2)
-    pred.append(sess.run(predictions, feed_dict={input_data: test_data, input_label: test_label})[0])
-pred = np.asarray(pred)
-test_label = np.asarray(f_test['m1m2'])
-m1 = np.mean(np.divide(abs(pred.T[0]-test_label.T[0]),test_label.T[0]))
-m2 = np.mean(np.divide(abs(pred.T[1]-test_label.T[1]),test_label.T[1]))
-print('with shift--m1: '+str(m1)+' m2: '+str(m2))
-showplot(pred,'shift')
+snr = np.linspace(40.0,1.5,386)
+m1s = []
+m2s = []
+for i in range(len(snr)):
+	pred = []
+	for j in range(len(f_test['WhitenedSignals'])):
+		test_data = f_test['WhitenedSignals'][j][start:end].reshape(1,end-start)
+		test_data = noise.add_shift(test_data)
+		if real_noise is False:
+				test_data = noise.add_noise(input=test_data, SNR=snr[i])
+		else:
+			test_data = noise.add_real_noise(input=test_data, SNR=snr[i])
+		test_data = test_data.reshape(1,end-start,1)
+		test_label = f_test['m1m2'][j].reshape(1,2)
+		pred.append(sess.run(predictions, feed_dict={input_data: test_data, input_label: test_label})[0])
+	pred = np.asarray(pred)
+	test_label = np.asarray(f_test['m1m2'])
+	m1 = np.mean(np.divide(abs(pred.T[0]-test_label.T[0]),test_label.T[0]))
+	m2 = np.mean(np.divide(abs(pred.T[1]-test_label.T[1]),test_label.T[1]))
+	m1s.append(m1)
+	m2s.append(m2)
+	print('SNR: '+str(snr[i])+' -- m1: '+"{0:.5%}".format(m1)+' m2: '+"{0:.5%}".format(m2))
+	if i % 50 == 0:
+		showplot(pred,'SNR--'+str(snr[i]))
+
+m1s = np.asarray(m1s)
+m2s = np.asarray(m2s)
+plt.figure()
+plt.plot(np.flip(snr),m1s*100)
+plt.plot(np.flip(snr),m2s*100)
+plt.legend(['m1','m2'], loc=1)
+plt.xlabel('SNR')
+plt.ylabel('Relative Error')
+plt.title('RE with SNR')
+plt.savefig(str(real_noise)+'SNR.png')
