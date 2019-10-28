03dae5622379be263fce22fb4f3cccccdde075cb
Jiangran Wang <wjr@wangjiangrandeMacBook-Air.local>
2019-10-28 09:45:57 -0500

    add infer

--- infer.py
+++ infer.py
@@ -1,4 +1,3 @@
-import datetime
 import tensorflow as tf
 import numpy as np
 import h5py
@@ -9,37 +8,30 @@ import sys
 import argparse
 from Noiser import Noiser
 from Net import WaveNet
-from Batch import get_batch, get_val
 import os
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
 parser = argparse.ArgumentParser(description='GW code')
-parser.add_argument('--train', dest='train_file', type=str, default='data/oneSecondTrain.h5',
-                    help='the file of the training data')
-parser.add_argument('--test', dest='test_file', type=str, default='data/oneSecondTest.h5',
+parser.add_argument('--model', dest='model_file', type=str, default='test1/model/True_R1noise.ckpt',
+                    help='the file of the model')
+parser.add_argument('--infer', dest='infer_file', type=str, default='data/oneSecondTest.h5',
                     help='the file of the testing data')
-parser.add_argument('--test_num', dest='test_num', type=str, default='1',
-                    help='test number')
+parser.add_argument('--name', dest='test_num', type=str, default='1on1',
+                    help='infer on model')
 parser.add_argument('--file', dest='file', type=bool, default=False,
                     help='whether cast output to a file')
-parser.add_argument('--snr_step', dest='snr_step', type=int, default=10,
-                    help='how many steps does each snr train')
 args = parser.parse_args()
 
 
-train_path = args.train_file
-test_path = args.test_file
-test_num = args.test_num
-snr_step = args.snr_step
+model_path = args.model_file
+infer_path = args.infer_file
+test_num = args.name
 if args.file:
     stdoutOrigin=sys.stdout 
     sys.stdout = open("testOut"+test_num+".txt", "w")
 
-f_train = h5py.File(train_path, "r")
-f_test = h5py.File(test_path, "r")
-NUM_DATA = f_train['data'].shape[0]
-assert NUM_DATA == 9840
-LENGTH = f_train['data'].shape[1]
+f_infer = h5py.File(infer_path, "r")
+LENGTH = f_infer['data'].shape[1]
 
 tf.logging.set_verbosity(tf.logging.ERROR)
 if np.isnan(f_train['data']).any():
@@ -76,54 +68,7 @@ sess = tf.Session(config=config)
 sess.run(init)
 loss_hist = []
 val_loss = []
-#saver.restore(sess, "../model/False_R1noise.ckpt")
-
-start = datetime.datetime.now()
-batch_size = 64
-real_noise = True  #change here!
-rate = 0.001
-# len(snr) = 50
-low = [0.6,0.5,0.4,0.4,0.3,0.3,0.3,0.2,0.2,0.2,0.1,0.1]
-snrs = [5.0,4.0,3.0,2.0,1.7,1.5,1.4,1.3,1.2,1.1,1.0,0.9,0.8,0.7] + [lows for lows in low for i in range(3)]
-num_epoch = int(snr_step * len(snrs))
-for i in range(num_epoch):
-    snr = snrs[i//snr_step]
-    # global_step.eval(session=sess)
-    train_data, train_label = get_batch(f_train, batch_size, length=LENGTH, real_noise=real_noise, SNR=snr)
-    for j in range(len(train_data)):
-        cur_data = train_data[j]
-        cur_label = train_label[j]
-        _, loss_val = sess.run([train_op, loss],
-                           feed_dict={input_data: cur_data,
-                                      input_label: cur_label,
-                                      trainable: True})
-        loss_hist.append(loss_val)
-        if j % 10 == 0:
-            print('loss: '+str(loss_hist[-1]))
-    
-    val_data, val_label = get_val(f_test, batch_size, length=LENGTH, real_noise=real_noise, SNR=snr)
-    validation = sess.run(loss, feed_dict={input_data: val_data, input_label: val_label, trainable: False})
-    val_loss.append(validation)
-    print('iter num: '+str(i)+' snr: '+str(snr)+' loss: '+str(loss_hist[-1])+' val_loss: '+str(val_loss[-1]))
-    #if i % 500 == 0:
-    #   saver.save(sess, '../model/'+str(real_noise)+'_Rnoise.ckpt', global_step=i)
-    
-end = datetime.datetime.now()
-print('time: '+str(end-start))
-
-#save model
-save_path = saver.save(sess, '../model/'+str(real_noise)+'_R'+test_num+'noise.ckpt')
-print("Model saved in path: %s" % save_path)
-step = NUM_DATA//batch_size
-axis = np.arange(step-1, len(loss_hist), step)
-plt.figure()
-plt.plot(loss_hist)
-plt.scatter(axis, val_loss, c = 'red')
-plt.legend(['train_loss','val_loss'], loc=1)
-plt.title('loss history--total time: '+str(end-start))
-plt.xlabel('epochs')
-plt.ylabel('loss')
-plt.savefig(test_num+'testLoss.png')
+saver.restore(sess, model_path)
 
 
 def plot(sess, snrs, f, fig, shift=None):
@@ -237,12 +182,13 @@ def gradual(sess, snrs, f, fig, timeStamps):
 
 
 snrs = np.linspace(5.0,0.1,50)
-plot(sess, snrs, f_test, test_num+'0.0-1.0s')
-plot(sess, snrs, f_test, test_num+'0.7-0.9s', shift=[int(LENGTH*0.7),int(LENGTH*0.9)])
-plot(sess, snrs, f_test, test_num+'0.5-1.0s', shift=[int(LENGTH*0.5),int(LENGTH*1.0)])
+plot(sess, snrs, 'infer'+f_infer, test_num+'0.0-1.0s')
+plot(sess, snrs, 'infer'+f_infer, test_num+'0.7-0.9s', shift=[int(LENGTH*0.7),int(LENGTH*0.9)])
+plot(sess, snrs, 'infer'+f_infer, test_num+'0.5-1.0s', shift=[int(LENGTH*0.5),int(LENGTH*1.0)])
 
 snrs = np.array([5.0,3.0,2.0,1.5,1.0,0.7,0.5,0.3,0.2,0.1])
 timeStamps = np.array([0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0])
-gradual(sess, snrs, f_test, 'test'+test_num+'-', timeStamps)
+gradual(sess, snrs, f_infer, 'infer'+test_num+'-', timeStamps)
 
-plot(sess, snrs, f_test, test_num+'zeroInput', shift=[0,0])
+plot(sess, snrs, f_infer, test_num+'zeroInput', shift=[0,0])
+
