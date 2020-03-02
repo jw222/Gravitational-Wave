61d4aa50821bc51100b668559d0d731a9f57bf9c
Jiangran Wang <wjr@wangjiangrandeMacBook-Air.local>
2020-03-02 13:26:05 -0600

    change size

--- classify.py
+++ classify.py
@@ -56,7 +56,7 @@ loss = tf.losses.softmax_cross_entropy(input_label, predictions)
 
 # train operation
 global_step = tf.Variable(0, trainable=False)
-optimizer = tf.train.RMSOptimizer(learning_rate=0.001)
+optimizer = tf.train.RMSPropOptimizer(learning_rate=0.001)
 train_op = optimizer.minimize(
     loss=loss,
     global_step=global_step)
