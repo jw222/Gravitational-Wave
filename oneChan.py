da2b8ff7af13f972b799ecdfe43aaf78e060c857
Jiangran Wang <wjr@wangjiangrandeMacBook-Air.local>
2020-04-29 01:51:17 -0500

    add two chan

--- oneChan.py
+++ oneChan.py
@@ -6,6 +6,7 @@ from net import WaveNet
 from utils import *
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
+
 def parseInput():
     # parsing argument
     parser = argparse.ArgumentParser(description='Training')
@@ -15,12 +16,12 @@ def parseInput():
                         help='the file of the testing data')
     parser.add_argument('--model', dest='model_path', type=str, default=None,
                         help='pretrained model')
-    parser.add_argument('--model_H', dest='model_H', type=str, default=None,
-                        help='pretrained model of channel H')
-    parser.add_argument('--model_L', dest='model_L', type=str, default=None,
-                        help='pretrained model of channel L')
     parser.add_argument('--event', dest='event', type=str, default='150914',
                         help='the event we want to train on')
+    parser.add_argument('--channel', dest='channel', type=str, default='H',
+                        help='the psd that calculated from that event')
+    parser.add_argument('--freq', dest='freq', type=int, default=8,
+                        help='frequency used')
     parser.add_argument('--num_filters', dest='num_filters', type=int, default=256,
                         help='number of filters used in residual blocks')
     parser.add_argument('--num_residuals', dest='num_residuals', type=int, default=6,
@@ -42,68 +43,50 @@ def parseInput():
     return parser.parse_args()
 
 
-def generator(file_name, event, ratio, pSNR=None):
+def generator(file_name, file_prefix, ratio, pSNR=None):
     file_name = file_name.decode('utf-8')
-    event = event.decode('utf-8')
-    with open('psd/' + event + 'H8freqs', 'rb') as fh:
-        freqs_H = pickle.load(fh)
-    with open('psd/' + file_prefix + 'H8pxx', 'rb') as fh:
-        pxx_H = pickle.load(fh)
-        interp_psd_H = interp1d(freqs, pxx)
-    with open('psd/' + event + 'L8freqs', 'rb') as fh:
-        freqs_L = pickle.load(fh)
-    with open('psd/' + file_prefix + 'L8pxx', 'rb') as fh:
-        pxx_L = pickle.load(fh)
-        interp_psd_L = interp1d(freqs, pxx)
+    file_prefix = file_prefix.decode('utf-8')
+    with open('psd/' + file_prefix + 'freqs', 'rb') as fh:
+        freqs = pickle.load(fh)
+    with open('psd/' + file_prefix + 'pxx', 'rb') as fh:
+        pxx = pickle.load(fh)
+        interp_psd = interp1d(freqs, pxx)
 
-    f_noise_H = h5py.File('data/' + event + 'H8Noise.h5', 'r')
-    f_noise_L = h5py.File('data/' + event + 'L8Noise.h5', 'r')
+    f_noise = h5py.File('data/' + file_prefix + 'Noise.h5', 'r')
     f_train = h5py.File(file_name, 'r')
     for waveform in f_train['Input']:
         waveform = decimate(waveform, 2)[-8192:]
         if np.random.uniform() < ratio:
             # blank waveform
-            whitened_H = np.zeros(8192)
-            whitened_L = np.zeros(8192)
+            whitened = np.zeros(8192)
             target = np.zeros(8192)
         else:
-            whitened_H = whiten(waveform, interp_psd_H)
-            whitened_L = whiten(waveform, interp_psd_L)
+            whitened = whiten(waveform, interp_psd)
             shiftInt = np.random.randint(-1024, 1024)
-            whitened_H = np.roll(whitened_H, shiftInt)
-            whitened_L = np.roll(whitened_L, shiftInt)
+            whitened = np.roll(whitened, shiftInt)
             if shiftInt >= 0:
-                whitened_H[:shiftInt] = 0
-                whitened_L[:shiftInt] = 0
+                whitened[:shiftInt] = 0
             else:
-                whitened_H[shiftInt:] = 0
-                whitened_H[shiftInt:] = 0
-            whitened_H /= np.amax(np.absolute(whitened_H))
-            whitened_L /= np.amax(np.absolute(whitened_L))
-            merger = (np.argmax(whitened_H) + np.argmax(whitened_L)) / 2.
+                whitened[shiftInt:] = 0
+            whitened /= np.amax(np.absolute(whitened))
+            merger = np.argmax(whitened)
             target = np.zeros(8192)
             target[max(shiftInt, 0):merger] = 1.0
-        noiseInt = np.random.randint(20, len(f_noise_H['noise']) - 8192)
-        noise_H = f_noise_H['noise'][noiseInt:noiseInt+8192]
-        noiseInt = np.random.randint(20, len(f_noise_L['noise']) - 8192)
-        noise_L = f_noise_L['noise'][noiseInt:noiseInt+8192]
+        noiseInt = np.random.randint(20, len(f_noise['noise']) - 8192)
+        noise = f_noise['noise'][noiseInt:noiseInt+8192]
         if pSNR is None:
             pSNR = np.random.uniform(0.5, 1.5)
-        noise_H /= np.std(noise_H)
-        noise_L /= np.std(noise_L)
-        output_H = whitened_H * pSNR + noise_H
-        output_L = whitened_L * pSNR + noise_L
-        output_H /= np.std(output_H)
-        output_L /= np.std(output_L)
-        output = np.stack([output_H, output_L], axis=0)
-        output = np.reshape(output, (2, 8192, 1))
+        noise /= np.std(noise)
+        output = whitened * pSNR + noise
+        output /= np.std(output)
+        output = output.reshape(8192, 1)
         yield output, target
 
 
 if __name__ == '__main__':
     args = parseInput()
-    fileCheck(event + 'H8')
-    fileCheck(event + 'L8')
+    prefix = args.event + args.channel + str(args.freq)
+    fileCheck(prefix)
     gpus = tf.config.experimental.list_physical_devices('GPU')
     print('number of gpus: ', len(gpus))
 
@@ -113,22 +96,22 @@ if __name__ == '__main__':
 
     train_dataset = tf.data.Dataset.from_generator(generator,
                                                    (tf.float64, tf.float64),
-                                                   ((2, 8192, 1), 8192),
-                                                   (args.train_file, args.event, args.blank_ratio))
+                                                   ((8192, 1), 8192),
+                                                   (args.train_file, prefix, args.blank_ratio))
     train_dataset = train_dataset.repeat(2).shuffle(buffer_size=1024).batch(args.batch_size)
     validation_dataset = tf.data.Dataset.from_generator(generator,
                                                         (tf.float64, tf.float64),
-                                                        ((2, 8192, 1), 8192),
-                                                        (args.test_file, args.event, args.blank_ratio))
+                                                        ((8192, 1), 8192),
+                                                        (args.test_file, prefix, args.blank_ratio))
     validation_dataset = validation_dataset.shuffle(buffer_size=1024).batch(args.batch_size)
 
     log_dir = './logs/' + args.output + '/'
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-    model = TwoChan(args.model_H, args.model_L, args.num_residuals, args.num_filters)
+    model = WaveNet(args.num_residuals, args.num_filters)
     tf.keras.backend.clear_session()
 
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=args.learning_rate),
-                  loss=tf.keras.losses.MeanSquaredError())
+                  loss=tf.keras.losses.BinaryCrossentropy())
 
     if args.model_path is not None:
         model.load_weights(args.model_path)
@@ -187,4 +170,3 @@ if __name__ == '__main__':
     peaks = testReal(model, prefix[:-1], 8192*10, 4096, args.output, 4096)
     for key in peaks:
         print(key, ': ', peaks[key])
-
